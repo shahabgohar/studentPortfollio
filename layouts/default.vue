96a5d11b1bab430dde8d7ca6fde7c860109ede1c
shahabgohar <shahab.developer.work@gmail.com>
2022-06-29 22:47:46 +0500

    added nuxt with vuetify

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+    </v-app-bar>
+    <v-main class="d-flex align-center justify-center">
+        <Nuxt />
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      fixed: false,
+      miniVariant: false,
+      right: false,
+      rightDrawer: false,
+      title: ''
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+
+      <v-sheet
+      color="transparent"
+      width="auto"
+        class="logo py-4 d-flex justify-center align-center flex-column text-center"
+      >
+        <v-card-text class="font-32px my-4">Building with Nuxt</v-card-text>
+        <NuxtLogo />
+        <v-card-text class="font-32px my-4">Building with </v-card-text>
+        <LoveLogo class="mb-4" />
+      </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
