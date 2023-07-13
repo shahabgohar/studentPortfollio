ae9acf093a0d9c091b947124627108c5c6b537e2
shahabgohar <shahab.developer.work@gmail.com>
2023-07-13 21:06:15 +0500

    added responsive icon in download resume button

--- components/ui/button.vue
+++ components/ui/button.vue
@@ -7,9 +7,9 @@ const props = defineProps({
 
 <template>
     <button class="flex items-center justify-center max-sm:px-3 max-sm:py-1 max-lg:px-6  py-3.5 px-12 rounded-md border border-black">
-            <div class="max-sm:text-xl max-lg:text-3xl text-4xl h-16 flex items-center justify-center space-x-4">
+            <div class="max-sm:text-xl max-lg:text-2xl text-4xl h-16 flex items-center justify-center space-x-4">
               <div class="">{{ title }}</div>
-              <Icon v-if="iconName" :name="iconName"></Icon>
+              <Icon v-if="iconName" class="max-md:w-6 max-md:h-6 max-lg:w-9 max-lg:h-9 w-11 h-11" :name="iconName"></Icon>
             </div>
     </button>
 </template>
